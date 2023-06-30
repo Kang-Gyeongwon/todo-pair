5e0e2eaa5c5fe4aafb192a9dd991be37c4de7a0f
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-30 11:13:27 +0900

    chore: 글자 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/1" element={<div>안녕하세요</div>} />
       </Routes>
     </BrowserRouter>
   );
